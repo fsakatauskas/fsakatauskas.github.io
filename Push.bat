46e45be96225f3bf5523364f8cc0bd74b01db3a5
fsakatauskas <fsakatauskas@rdisoftware.com>
2018-04-05 12:05:55 -0300

    1

--- Push.bat
+++ Push.bat
@@ -1,3 +1,3 @@
 git add .
 git commit -am "1"
-git push https://fsakatauskas@github.com:5e7a32ab01729fe3cd7dbeddc2213186b897a11b/fsakatauskas/fsakatauskas.github.io
+git push https://fsakatauskas@github.com/fsakatauskas/fsakatauskas.github.io
